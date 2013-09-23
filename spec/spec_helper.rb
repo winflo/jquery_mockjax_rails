3f70371b7e72fe641c00d3a24159eb277986f52e
Heiko Zeus <heiko.zeus@zweitag.de>
2013-09-23 09:15:24 +0200

    Adjust tests to removed rspec_integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'capybara/rspec'
 require 'capybara/dsl'
 require 'jquery_mockjax_rails'
 
+require 'jquery_mockjax_rails/rspec_integration'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
